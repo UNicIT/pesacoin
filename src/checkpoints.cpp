c48391d713326800a50658b982722ee0212a0a6a
Simon <seruyinda@gmail.com>
2017-04-15 14:56:00 +0400

    Main Net genesis block

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -24,7 +24,7 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of // Yo dawg, this is the secret. Checkpoint 0 hash == Genesis block hash.
-        (         0, uint256("0xbf9385c680c3a7aec778b6dcf87dbfb105369d55143fc99ebe86f469cd53ddca"))
+        (         0, uint256("0x985a155532dc290adaa4a570f3eace7ff5c4b3ae67f2c3bb447e90ef2ca61cc9"))
         ;
 
     bool CheckBlock(int nHeight, const uint256& hash)
--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,8 @@ CCriticalSection cs_main;
 CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
-map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0xbf9385c680c3a7aec778b6dcf87dbfb105369d55143fc99ebe86f469cd53ddca");
+map<uint256, CBlockIndex*> mapBlockIndex; 
+uint256 hashGenesisBlock("0x985a155532dc290adaa4a570f3eace7ff5c4b3ae67f2c3bb447e90ef2ca61cc9");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
@@ -2014,9 +2014,9 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1492243409; //epochtime
+        block.nTime    = 1492251683; //epochtime
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 0;
+        block.nNonce   = 545355;
 
         if (fTestNet)
         {
@@ -2024,11 +2024,11 @@ bool LoadBlockIndex(bool fAllowNew)
             block.nNonce   = 0;
         }
 
-        //// debug print 2fc1b7ef46270c053711fbae934cf7f83378efd4b3e158079451d9c6c90e4700
+        //// debug print 2fc1b7ef46270c053711fbae934cf7f83378efd4b3e158079451d9c6c90e4700 testnet
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x2fc1b7ef46270c053711fbae934cf7f83378efd4b3e158079451d9c6c90e4700"));
+        assert(block.hashMerkleRoot == uint256("0x30f45161daf297abf43af51debc254d3d616b34a7cbf78030eae4c7c973e959e"));
 
         // If genesis block hash does not match, then generate new genesis hash.
         if (false && block.GetHash() != hashGenesisBlock)
